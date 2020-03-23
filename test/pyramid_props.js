ede37d938fa1e34a562e009121c3b3333b51a53b
Paul Rohorzka <paul@paulroho.com>
2020-03-23 22:52:36 +0100

    BACKUP

--- test/pyramid_props.js
+++ test/pyramid_props.js
@@ -3,6 +3,8 @@ const esmRequire = require('esm')(module)
 const pyramid = esmRequire('../pyramid.js')
 const { getPoints } = pyramid
 
+fc.configureGlobal({ numRuns: 10 });
+
 describe('The triangle on top', () => {
     it('should have 3 vertices', () => {
         assertTrianglePoints(trianglePoints => {
@@ -21,7 +23,7 @@ describe('The triangle on top', () => {
             const tip = trianglePoints[0];
             const bottomLeft = trianglePoints[1];
             const bottomRight = trianglePoints[2];
-            return tip.x - bottomLeft.x === bottomRight.x - tip.x;
+            return Math.abs((tip.x - bottomLeft.x) - (bottomRight.x - tip.x)) < 1e-6;
         });
     })
 });
@@ -52,7 +54,7 @@ describe('The trapezoid at the base', () => {
             const topRight = trapezoidPoints[1];
             const bottomRight = trapezoidPoints[2];
             const bottomLeft = trapezoidPoints[3];
-            return topLeft.x - bottomLeft.x == bottomRight.x - topRight.x;
+            return Math.abs((topLeft.x - bottomLeft.x) - (bottomRight.x - topRight.x)) < 1e-6;
         });
     });
 });
@@ -88,9 +90,29 @@ describe('The triangle and the trapezoid', () => {
             const trapezoidDeltaX = trapezoidBottomRight.x - trapezoidTopRight.x;
             // console.log('Trapezoid: ' + trapezoidHeight + ', ' + trapezoidDeltaX + ': ' + trapezoidHeight/trapezoidDeltaX);
 
-            return triangleHeight * trapezoidDeltaX === trapezoidHeight * triangleDeltaX;
+            return Math.abs(triangleHeight * trapezoidDeltaX - trapezoidHeight * triangleDeltaX) < 1e-7;
             // return triangleHeight/triangleDeltaX === trapezoidHeight/trapezoidDeltaX;
         });
+    });
+    it('should have areas reflecting the two numbers given', () => {
+        return fc.assert(
+            fc.property(
+                fc.nat(), fc.nat(), (n1, n2) => {
+                    const points = getPoints(n1, n2);
+
+                    const trianglePoints = points[0];
+                    const triangleArea = calcPolygonArea(trianglePoints);
+
+                    const trapezoidPoints = points[1];
+                    const trapezoidArea = calcPolygonArea(trapezoidPoints);
+
+                    console.log('----');
+                    console.log(n2 / n1);
+                    console.log(trapezoidArea / triangleArea);
+                    return Math.abs(n2 / n1 - trapezoidArea / triangleArea) < 1e-7;
+                }
+            )
+        );
     });
 });
 
@@ -101,7 +123,8 @@ const pointsAreEqual = (p1, p2) => {
 const assertPyramidPoints = (assertion) => {
     return fc.assert(
         fc.property(
-            fc.integer(), fc.integer(), (n1, n2) => {
+            fc.integer(1, 10000), fc.nat(), (n1, n2) => {
+                console.log('n1: ' + n1 + ', n2: ' + n2);
                 const points = getPoints(n1, n2);
                 return assertion(points);
             }
@@ -119,4 +142,20 @@ const assertTrapezoidPoints = (assertion) => {
     return assertPyramidPoints(points => {
         return assertion(points[1]);
     });
+};
+
+const calcPolygonArea = (vertices) => {
+    var total = 0;
+
+    for (var i = 0, l = vertices.length; i < l; i++) {
+        var addX = vertices[i].x;
+        var addY = vertices[i == vertices.length - 1 ? 0 : i + 1].y;
+        var subX = vertices[i == vertices.length - 1 ? 0 : i + 1].x;
+        var subY = vertices[i].y;
+
+        total += (addX * addY * 0.5);
+        total -= (subX * subY * 0.5);
+    }
+
+    return Math.abs(total);
 };
