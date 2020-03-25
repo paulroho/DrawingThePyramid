ac6f33f260cc956152d75cf296573ae6bfc92c4d
Paul Rohorzka <paul.rohorzka@techtalk.at>
2020-03-25 23:18:30 +0100

    Use viewbox to center the pyramid around the y-axis

--- pyramid.js
+++ pyramid.js
@@ -2,7 +2,6 @@ export function getPoints(topCount, bottomCount) {
     const f = Math.sqrt(topCount / (topCount + bottomCount));
     const b = 400;
     const h = 400;
-    const mid = 250;
 
     const n = h * f;
     const c = b * f;
@@ -10,15 +9,15 @@ export function getPoints(topCount, bottomCount) {
     // TODO Implement here
     return [
         [
-            { x: mid, y: 50 },
-            { x: mid - c / 2, y: 50 + n },
-            { x: mid + c / 2, y: 50 + n }
+            { x: 0, y: 50 },
+            { x: -c / 2, y: 50 + n },
+            { x: +c / 2, y: 50 + n }
         ],
         [
-            { x: mid - c / 2, y: 50 + n },
-            { x: mid + c / 2, y: 50 + n },
-            { x: mid + 200, y: 450 },
-            { x: mid - 200, y: 450 }
+            { x: -c / 2, y: 50 + n },
+            { x: +c / 2, y: 50 + n },
+            { x: +200, y: 450 },
+            { x: -200, y: 450 }
         ]
     ];
 }
