b16f3320a5d1212d7589596c0bd940673af6f40a
Paul Rohorzka <paul@paulroho.com>
2020-03-29 16:19:42 +0200

    Add simple UI for options

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const count2Text = document.getElementById('number-2');
 const classification1Text = document.getElementById('classification-1');
 const classification2Text = document.getElementById('classification-2');
 
+const optionTopText = document.getElementById('option-top');
+const optionHeightText = document.getElementById('option-height');
+const optionWidthText = document.getElementById('option-width');
+
 const topPart = document.getElementById('part-top');
 const topCountText = topPart.getElementsByClassName('test-count')[0];
 const topClassificationText = topPart.getElementsByClassName('test-classification')[0];
@@ -26,6 +30,10 @@ function wire() {
 
     count1Text.onchange = redraw;
     count2Text.onchange = redraw;
+
+    optionTopText.onchange = redraw;
+    optionHeightText.onchange = redraw;
+    optionWidthText.onchange = redraw;
 }
 
 function updateCaptions() {
@@ -37,14 +45,17 @@ function redraw() {
     const topCount = parseInt(count1Text.value);
     const bottomCount = parseInt(count2Text.value);
 
+    const top = parseInt(optionTopText.value);
+    const height = parseInt(optionHeightText.value);
+    const width = parseInt(optionWidthText.value);
+
     topCountText.textContent = topCount;
     bottomCountText.textContent = bottomCount;
 
-    updatePyramid(topCount, bottomCount);
+    updatePyramid(topCount, bottomCount, {top, height, width});
 }
 
-function updatePyramid(topCount, bottomCount) {
-    const options = { top:50, height:400, width:400 };
+function updatePyramid(topCount, bottomCount, options) {
     const points = pyramid.getPoints(topCount, bottomCount, options);
 
     setPoints(topShape, points[0]);
